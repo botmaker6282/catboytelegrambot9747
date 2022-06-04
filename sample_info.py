2f10fe7b6f17ccafb534ddf2099319d6790d1a06
botmaker6282 <106882347+botmaker6282@users.noreply.github.com>
2022-06-05 02:03:34 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,10 +1,10 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'catboytelegrambot9747'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
 API_HASH = '0123456789abcdef0123456789abcdef'
 BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+USERBOT_STRING_SESSION = 'cat-boy-telegram-bot'
 
 # Bot settings
 CACHE_TIME = 300
@@ -12,7 +12,7 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+CHANNELS = [-10012345678, -100987654321, 'catboy123']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
